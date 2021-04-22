fc8f4ab8d03132493b66d6b860075a6a249d7691
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2021-04-22 10:15:38 -0500

    better documentation

--- fcc.py
+++ fcc.py
@@ -1,18 +1,28 @@
 from math import log10, sqrt
 
 def pth(d,f):
-    """d in cm, f in GHz. Source: FCC 19-126 p.23"""
+    """Calculate time-averaged power threshold for exemption, for radio
+    frequency sources. (Exemption from routine RF exposure
+    evaluation.) Note, this is only for UHF and part of SHF. Not
+    applicable to VHF, HF, and frequencies below. FCC formula is
+    'based on localized specific absorption rate (SAR) limits.' For
+    lower frequencies, you need to look at MPE-based exemption with
+    ERP threshold, not SAR-based.
+
+    d in cm, f in GHz. Return value in mW. Source: FCC 19-126 p.23
+
+    """
     if 0.3 <= f < 1.5:
         erp20 = 2040 * f
     elif 1.5 <= f <= 6:
         erp20 = 3060
     else:
-        raise Exception('frequency out of range: ' + str(f))
+        raise Exception('frequency out of range: ' + str(f) + ' GHz')
     x = -1 * log10(60 / (erp20 * sqrt(f)))
     if d <= 20:
         P_threshold = erp20 * (d / 20) ** x
     elif 20 < d <= 40:
         P_threshold = erp20
     else:
-        raise Exception('distance out of range: ' + str(d))
+        raise Exception('distance out of range: ' + str(d) + ' cm')
     return(P_threshold)

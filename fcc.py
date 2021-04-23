a3e8e538e697d7a2d71709b1761c120555d78736
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2021-04-23 09:43:48 -0500

    PEP and other refactors via PyCharm.

--- fcc.py
+++ fcc.py
@@ -1,6 +1,7 @@
 import math
 
-def pth(d,f):
+
+def pth(d, f):
     """Calculate time-averaged power threshold for exemption, for radio
     frequency sources. (Exemption from routine RF exposure
     evaluation.) Note, this is only for UHF and part of SHF. Not
@@ -10,7 +11,6 @@ def pth(d,f):
     ERP threshold, not SAR-based.
 
     d in cm, f in GHz. Return value in mW. Source: FCC 19-126 p.23
-
     """
     if 0.3 <= f < 1.5:
         erp20 = 2040 * f
@@ -20,28 +20,38 @@ def pth(d,f):
         raise ValueError('frequency out of range: ' + str(f) + ' GHz')
     x = -1 * math.log10(60 / (erp20 * math.sqrt(f)))
     if d <= 20:
-        P_threshold = erp20 * (d / 20) ** x
+        p_threshold = erp20 * (d / 20) ** x
     elif 20 < d <= 40:
-        P_threshold = erp20
+        p_threshold = erp20
     else:
         raise ValueError('distance out of range: ' + str(d) + ' cm')
-    return(P_threshold)
+    return p_threshold
 
 
-def erpth(d,f):
+def erpth(d, f):
     """ d in meters, f in MHz, return val in watts. """
     cutpoints = [0.3, 1.34, 30, 300, 1500, 100000]
-    def f1(f,r): return(1920 * r**2)
-    def f2(f,r): return(33450 * r**2 / f**2)
-    def f3(f,r): return(3.83 * r**2)
-    def f4(f,r): return(0.0128 * r**2 * f)
-    def f5(f,r): return(19.2 * r**2)
+
+    def f1(f, r):
+        return 1920 * r ** 2
+
+    def f2(f, r):
+        return 33450 * r ** 2 / f ** 2
+
+    def f3(f, r):
+        return 3.83 * r ** 2
+
+    def f4(f, r):
+        return 0.0128 * r ** 2 * f
+
+    def f5(f, r):
+        return 19.2 * r ** 2
+
     functions = [f1, f2, f3, f4, f5]
-    c = 299792458    # m/s
-    nu = f * 1E6     # Hz
+    c = 299792458  # m/s
+    nu = f * 1E6  # Hz
     lambd = c / nu  # m
-    L2p = lambd / (2 * math.pi)
-    if d < L2p:
+    if d < lambd / (2 * math.pi):
         print('R=' + str(d) + ', L/(2pi) = ' + str(round(L2p, 2)) + 'm')
         raise ValueError('R < L/(2pi), therefore RF evaluation required.')
     for i in range(len(cutpoints)):
@@ -50,5 +60,5 @@ def erpth(d,f):
         f_low = cutpoints[i]
         f_high = cutpoints[i + 1]
         if f_low <= f < f_high:
-            return(functions[i](f, d))
+            return functions[i](f, d)
     raise ValueError('Reached end; freq not in table: ' + str(f) + ' MHz')

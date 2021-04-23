972278067f0922d1f7c9b8c8bbdb5605b44649b5
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2021-04-23 13:20:40 -0500

    New test lines 19, 26. Thanks pytest-cov.

--- test_fcc.py
+++ test_fcc.py
@@ -34,6 +34,7 @@ def test_all_sar():
         print()
     print(result_list)
     assert result_list == reference
+    assert fcc_round(exempt_milliwatts_sar(40, 1.8)) == 3060
 
 def t_erpth(d, f):
     erp = exempt_watts_mpe(d, f)
